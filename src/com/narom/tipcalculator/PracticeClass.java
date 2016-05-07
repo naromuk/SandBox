9d2109166f6aa232d9281baaf405a50aff2be2e6
Narom Kuoch <naromuk@gmail.com>
2016-05-06 22:17:43 -0500

    second commit

---
+++ src/com/narom/tipcalculator/PracticeClass.java
@@ -0,0 +1,18 @@
+package com.narom.tipcalculator;
+
+public class PracticeClass {
+
+    private Integer mInteger;
+
+    public PracticeClass(Integer num) {
+        mInteger = num;
+    }
+
+    public void setInt(Integer number) {
+        mInteger = number;
+    }
+
+    public Integer getInt() {
+        return mInteger;
+    }
+}
