f432653f0a337a5ec71bffcb4ad08c80d79660c8
mazorol <mazorol@gmail.com>
2016-08-07 15:21:06 -0500

    first comment

--- src/com/narom/tipcalculator/PracticeClass.java
+++ src/com/narom/tipcalculator/PracticeClass.java
@@ -7,7 +7,8 @@ public class PracticeClass {
     public PracticeClass(Integer num) {
         mInteger = num;
     }
-
+    // this is a test
+    
     public void setInt(Integer number) {
         mInteger = number;
     }
